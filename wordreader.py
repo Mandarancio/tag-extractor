6d53fba3188b9cf26dd69063b5e20f7bcc159589
Djavan <djavan.sergent@gmail.com>
2017-03-27 16:13:44 +0200

    validation

---
+++ wordreader.py
@@ -0,0 +1,15 @@
+from nltk.corpus import wordnet as wn
+from unidecode import unidecode
+
+
+class Wordreader:
+    def __init__(self):
+        pass
+
+    def rqst(self, word):
+        uni_word = unidecode(word)
+        synsets = wn.synsets(uni_word)
+        for s in synsets:
+            name = s.name()
+            lemmas = wn.synset(name).lemmas()
+            print(lemmas)

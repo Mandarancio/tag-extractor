24c8fb845f0db93893c7425d7f651d6ca33916dd
Djavan <djavan.sergent@gmail.com>
2017-03-30 14:31:43 +0200

    updated database manager example

--- examples/database.py
+++ examples/database.py
@@ -1,5 +1,26 @@
+#! /usr/bin/python3
+# @Djavan Sergent
 import sqlmanager as sql
 
 if __name__ == "__main__":
-    p = sql.Pictures()
-    print(p)
+
+    # Création de la base de données (Tables)
+    sql.Base.metadata.create_all(sql.engine)
+
+    # Création de l'instance de l'ORM
+    session = sql.Session()
+
+    # Création d'une instance de Pictures
+    pict = sql.Pictures(pict="4563-56667-324", tags="test", location="65.034 ; 0.847439")
+
+    # Persistance de l'instance dans l'ORM
+    session.add(pict)
+
+    # Modification de l'objet
+    pict.location = "65.093434 ; 98.0293984"
+
+    # Commit des modifications dans la base de données
+    session.commit()
+
+    # Appel de la fonction __repr__ de l'objet
+    print(pict)
--- sqlmanager.py
+++ sqlmanager.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python3
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Sequence, create_engine
 
 Base = declarative_base()
+engine = create_engine('sqlite:///:memory:', echo=True)
+Session = sessionmaker(bind=engine)
 
 
 class Pictures(Base):
@@ -11,9 +14,10 @@ class Pictures(Base):
     """
     __tablename__ = 'pictures'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence('pictures_id_seq'), primary_key=True)
+    pict = Column(String)
     tags = Column(String)
     location = Column(String)
 
     def __repr__(self):
-        return "<TAGS(id='%s', tags='%s', location='%s')>" %(self.id, self.tags, self.location)
+        return "<Picture(id='%s', pict='%s', tags='%s', location='%s')>" %(self.id, self.pict, self.tags, self.location)

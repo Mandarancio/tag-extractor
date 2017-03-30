eff0fdb7fbd6aa6341cf9591c46845f39c7cbfa1
Martino Ferrari <manda.mgf@gmail.com>
2017-03-30 09:36:59 +0200

    updated using new api of instasplitter

--- demo_split.py
+++ demo_split.py
@@ -3,7 +3,7 @@ import instasplitter
 print('loading demo....')
 demoExt = exs.JsonExtractor('demophotos.json')
 print('loading splitter....')
-splitter = instasplitter.HashtagSplitter()
+splitter = instasplitter.HashtagSplitter('freqs.json')
 print('splitting....')
 for p in demoExt.get_tags(0, 0, 1, -1):
     for t in p['tags']:

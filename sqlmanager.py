4651ae3f4182fe95bd03c2f6c6f86e5df69102df
Djavan <djavan.sergent@gmail.com>
2017-03-30 13:32:25 +0200

    sqlmanager test

---
+++ sqlmanager.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class Pictures(Base):
+    __tablename__ = 'pictures'
+
+    id = Column(Integer, primary_key=True)
+    tags = Column(String)
+    location = Column(String)
+
+    def __repr__(self):
+        return "<TAGS(id='%s', tags='%s', location='%s')>" %(self.id, self.tags, self.location)
+
+
+if __name__ == "__main__":
+    p = Pictures()
+    print(p)

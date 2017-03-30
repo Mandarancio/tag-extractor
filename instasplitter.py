fd095409a0595c7e8ee798f2f6ceecd107657955
Martino Ferrari <manda.mgf@gmail.com>
2017-03-30 09:57:44 +0200

    Cleaner code

--- instasplitter.py
+++ instasplitter.py
@@ -1,15 +1,8 @@
 #! /usr/bin/python3
+# Martino Ferrari
 import re
 import time
 import json
-
-
-def pprint(tree, space=''):
-    if tree is None:
-        return
-    for l in tree:
-        print(space+l['term']+'('+str(l['freq'])+')')
-        pprint(l['rest'], '  '+space)
 
 
 class SimpleSplitter:
@@ -27,31 +20,28 @@ class SimpleSplitter:
 
 
 class HashtagSplitter:
+    '''
+    High-perforamnce Advanced hashtag splitter using frequency of possible word
+    combination to split correctly the tag.
+    @Martino Ferrari
+    :param freq_file: json frequency wordlist
+    '''
     def __init__(self, freq_file):
         fp = open(freq_file)
-        self.__most_10000__ = json.load(fp)
+        self.__freqs__ = json.load(fp)
         fp.close()
 
-    def __to_dic__(self, freqs):
-        res = {}
-        tot = 0
-        for l in freqs:
-            if len(l[0]) > 1:
-                res[l[0]] = l[1]
-                tot += l[1]
-            elif l[0] == 'a' or l[0] == 'i':
-                res[l[0]] = l[1]
-                tot += l[1]
-        for k in res:
-            res[k] /= tot
-        return res
-
     def parse_tag(self, term):
+        '''
+        parse a tag, first step use standarad delimiter, numbers to split
+        then it use the frequency
+        :param term: tag to parse (no hash please)
+        :return: tag splitted in a list
+        '''
         words = []
         # Remove hashtag, split by dash
         term = term.replace('-', ' ').replace('_', ' ').replace('+', ' ')
-        tags = re.sub(r"([0-9]+)", r" \1 ",
-                      re.sub(r"([A-Z])", r" \1", term)).split()
+        tags = re.sub(r"([0-9]+)", r" \1 ", term).split()
         for tag in tags:
             if len(tag) <= 2 or len(tag) > 20:
                 words.append(tag.lower())
@@ -61,15 +51,17 @@ class HashtagSplitter:
         return words
 
     def recursive_split(self, term):
-        if self.find_word(term):
-            max_freq = self.freq(term)
-        else:
-            max_freq = 0
+        '''
+        this is the actual magic of the class ;)
+        :param term: all low case tag to split
+        :return: most probable bag of words
+        '''
+        max_freq = self.freq(term)
         res = [term]
         for i in range(len(term), 0, -1):
             word = term[0:i]
-            if self.find_word(word):
-                ff = self.freq(word)
+            ff = self.freq(word)
+            if ff > 0:
                 fr, rest = self.recursive_split(term[i:])
                 ff = ff*fr/2
                 if ff > max_freq:
@@ -79,21 +71,32 @@ class HashtagSplitter:
         return max_freq, res
 
     def freq(self, word):
-        return self.__most_10000__[word]
+        '''
+        return the frequency of a word
+        :param word: word to check
+        :return: frequency (0 if not in dictionary)
+        '''
+        if word not in self.__freqs__:
+            return 0
+        return self.__freqs__[word]
 
-    def find_word(self, token):
-        return token in self.__most_10000__
-
-
-def test(tag):
-    print('Hashtag: '+tag)
-    t = time.time()
-    print(splitter.parse_tag(tag))
-    t = time.time()-t
-    print(t)
+    def find_word(self, word):
+        '''
+        check if a word is the dictionary
+        :param word: word to check
+        :return: boolean
+        '''
+        return word in self.__freqs__
 
 
 if __name__ == "__main__":
+    def test(tag):
+        print('Hashtag: '+tag)
+        t = time.time()
+        print(splitter.parse_tag(tag))
+        t = time.time()-t
+        print(t)
+
     splitter = HashtagSplitter('resources/freqs.json')
     print('loaded')
     test('makeupartist')

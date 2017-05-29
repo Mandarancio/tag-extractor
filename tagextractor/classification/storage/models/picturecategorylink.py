38f74e29ee06a4bf4ab39ca600e2356b2905ea4a
Djavan <djavan.sergent@gmail.com>
2017-05-29 22:48:38 +0200

    new models

---
+++ tagextractor/classification/storage/models/picturecategorylink.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+"""Picture tag link database table manager
+author: Djavan Sergent
+"""
+import tagextractor.classification.storage.base as db
+from sqlalchemy import Column, Integer, ForeignKey
+
+
+# pylint: disable = too-few-public-methods
+class PictureCategoryLink(db.CLASSIFIED_BASE):
+    """A mapping class for link between Picture and Tag Object."""
+    __tablename__ = 'picture_category_link'
+
+    # Table fields
+    tag_id = Column(Integer, ForeignKey('tag.id'), primary_key=True)
+    picture_id = Column(Integer, ForeignKey('picture.id'), primary_key=True)
+
+    def __repr__(self):
+        """Print the picture tag link."""
+        return "<Link(pid='%s', tid='%s')>" \
+               % (self.picture_id, self.tag_id)

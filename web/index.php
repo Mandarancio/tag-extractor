35bb9080cd9e4f86a4f76eabacf09a8015454eb4
Sarra-PC\Sarra <graja.sarra@gmail.com>
2017-05-30 19:37:51 +0100

    evaluation code en php

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Tag-extractor</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <!-- Nécessaire pour le fonctionnement de la google map -->
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVgf3LOWvkoIpuVl2cG_uVDdDcQjDqc-k"></script>
+  
+  <!-- Import du css -->
+  <link href="css/index.css" rel="stylesheet">
+
+  <!-- Script javascript -->
+  <script type="text/javascript" src="js/map.js"></script>
+  
+</head>
+
+<body>
+
+  <div class="jumbotron text-center">
+    <h1>TAG-EXTRACTOR</h1>
+  </div>
+
+  <div id="map_geolocalisation" class="form-control"></div>
+
+</body>
+
+</html>
